e0ed5ef734c867e897bb24f2ee21498d5a4ee237
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-10-30 17:27:07 -0300

    Quitar el formato YYYY/MM/DD del patrón fecha.

--- src/sanatoriocolegiales/especificaciones/evento.clj
+++ src/sanatoriocolegiales/especificaciones/evento.clj
@@ -2,7 +2,7 @@
   (:require [clojure.spec.alpha :as s]
             [hyperfiddle.rcf :refer [tests]]))
 
-(def patron-fecha #"\d{4}-\d{2}-\d{2}|\d{4}/\d{2}/\d{2}")
+(def patron-fecha #"\d{4}-\d{2}-\d{2}")
 (s/def ::nombre string?)
 (s/def ::origen #{"uti" "uco" "cirugia" "convenios" "hcdm"})
 (s/def ::fecha (s/and string? #(re-matches patron-fecha %)))
@@ -11,7 +11,7 @@
  (s/valid? ::origen ::uti) := false
  (s/valid? ::origen "res")  := false
  (s/valid? ::fecha "1990-12-21") := true
- (s/valid? ::fecha "1000/12/21") := true
+ (s/valid? ::fecha "1000/12/21") := false
  (s/valid? ::fecha "100/12/21") := false
  (s/valid? ::fecha "1000/12/1") := false
  (s/valid? ::fecha "1000/12-21") := false
