6fb839ba6da4a45d96d32896a4bc4a2b207317b6
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-11-06 12:06:57 -0300

    Quitar cosas innecesarias

--- src/sanatoriocolegiales/logging_servoy/service.clj
+++ src/sanatoriocolegiales/logging_servoy/service.clj
@@ -1,32 +1,11 @@
-;; ---------------------------------------------------------
-;; sanatoriocolegiales.logging-servoy
-;;
-;; TODO: Provide a meaningful description of the project
-;;
-;; Start the service using donut system configuration
-;; defined in `system.clj`
-;;
-;; The service consist of
-;; - httpkit web application server
-;; - metosin/reitit for routing and ring for request / response management
-;; - mulog event logging service
-;;
-;; Related namespaces
-;; `sanatoriocolegiales.logging-servoy/system` donut system configuration
-;; ---------------------------------------------------------
-
-
 (ns sanatoriocolegiales.logging-servoy.service
-  "Gameboard service component lifecycle management"
+  "Servoy Log API service component lifecycle management"
   (:gen-class)
   (:require
    ;; Component system
    [donut.system           :as donut]
    [sanatoriocolegiales.logging-servoy.system :as system]))
 
-
-;; --------------------------------------------------
-;; Service entry point
 
 (defn -main
   "sanatoriocolegiales logging-servoy service managed by donut system,
@@ -45,24 +24,6 @@
     (.addShutdownHook
      (Runtime/getRuntime)
      (Thread. ^Runnable #(donut/signal running-system ::donut/stop)))))
-;; --------------------------------------------------
 
 
-;; --------------------------------------------------
-;; Example clojure.exec function
 
-(defn greet
-  "Greeting message via Clojure CLI clojure.exec"
-  ;; TODO: call greet with hash-map argument
-  ([] (greet "secret engineering"))
-  ([{:keys [team-name]}]
-   (str "sanatoriocolegiales logging-servoy service developed by the " team-name " team")))
-
-
-(comment
-  ;; --------------------------------------------------
-  ;; REPL workflow commands
-
-  (greet {:team-name "Jrivero"}))
-
-   ; End of rich comment
